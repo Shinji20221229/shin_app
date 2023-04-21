66e6f03993736d5dbff0e3538b7a2a6c134be485
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-04-21 09:30:11 +0000

    adminカラム修正

--- db/migrate/20230421091556_create_admins.rb
+++ db/migrate/20230421091556_create_admins.rb
@@ -4,6 +4,9 @@ class CreateAdmins < ActiveRecord::Migration[6.1]
 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,9 @@ ActiveRecord::Schema.define(version: 2023_04_21_092341) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
