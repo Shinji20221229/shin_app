88ed61f6445e4c8101ff670a5ada30499a0efccc
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-04-27 07:59:17 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   namespace :customers do
-    get 'homes/top' => 'homes#top'
+    root to: 'homes#top'
+
+    namespace :admin do
+    end
+
+    scope module: :public do
+    end
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
