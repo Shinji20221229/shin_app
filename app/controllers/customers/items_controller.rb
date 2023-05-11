a4950ad9e7e605092512af4fa4bda06ff20c4572
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-05-11 08:25:23 +0000

    commit

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,4 +1,11 @@
 class Customers::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @items = Item.page(params[:page])
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 end
